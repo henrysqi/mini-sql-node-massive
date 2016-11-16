e0d0297dff37b8924ede652605473ad5bd2757ff
Henry Qi <henryqi222@gmail.com>
2016-11-16 15:05:41 -0700

    complete

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var connectionString = "postgres://postgres:pgadminpass@localhost/sandbox";
+var massiveInstance = massive.connectSync({connectionString : connectionString})
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.set('db', massiveInstance);
+var db = app.get('db');
+
+
+
+var controller = require('./controller.js');
+
+controller.getPlanes();
+
+
+
 
 app.listen('3000', function(){
   console.log("Successfully listening on : 3000")	
